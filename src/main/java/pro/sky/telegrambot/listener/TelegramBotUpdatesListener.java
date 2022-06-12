52b895357898146588fde48aa91ed45393f45536
Awalone01 <sashaneiv01@yandex.ru>
2022-06-12 17:37:24 +0300

    commit 1

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,7 +2,9 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -24,13 +27,30 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
+    TelegramBot bot = new TelegramBot("BOT_TOKEN");
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            Message message = update.message();
+            if (message.text().startsWith("/start")) {
+                sendMsg(getChatId(message), "Привет, я робот");
+            }else{
+                    sendMsg(getChatId(message), "Я не знаю что ответить на это");
+            }
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private Long getChatId(Message message) {
+        return message.chat().id();
+    }
+
+    private void sendMsg(Long chatId, String s) {
+        SendMessage sendMessage = new SendMessage(chatId, s);
+        telegramBot.execute(sendMessage);
     }
 
 }
